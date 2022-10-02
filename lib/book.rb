84b83936dab6bd5708dc451682f7fcf36945cf0d
RhonaJoyKe <rhonajoy8@gmail.com>
2022-10-01 20:42:54 -0400

    commit tree d76d5b46685ff3454b832c68a8e5e59f3831874a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
+class Book
+    attr_accessor :title
+    attr_reader :author,:page_count,:genre
+    def initialize(title)
+       @title =title
+    
+        @author=author
+   
+        @page_count=page_count
+    
+        @genre=genre
+    
+    end
+end
 
